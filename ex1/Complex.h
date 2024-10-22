b0e77b69966f4babbbb534538eb78b3fd70528ca
ibtissam abdouni <167817121+ibtissam203@users.noreply.github.com>
2024-10-22 11:16:18 +0000

    Complex.h

--- ex1/Complex.h
+++ ex1/Complex.h
@@ -1,16 +1,35 @@
-#pragma once
-class Complex
-{
-private:
-	double Re;
-	double Img;
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <iostream>
+#include <cmath> // pour sqrt
+using namespace std;
+
+class Complexe {
+    double Re, Img;
+
 public:
-	Complex();
-	Complex(double, double);
-	Complex(const Complex&);
-	void afficher() const;
-	void afficher(double) const;
-	Complex operator + (const Complex& Z);
-	double modulo(Complex& R);
+    // Constructeurs
+    Complexe();
+    Complexe(double reel, double imaginaire);
+    Complexe(const Complexe& c);
+
+    // Méthodes
+    void afficher() const;
+    
+
+    // Surcharge des opérateurs
+    Complexe operator+(const Complexe& Z) ;
+    Complexe operator+(const double a) const;
+    Complexe operator-(const Complexe& Z) const;
+    Complexe operator*(const Complexe& R) const;
+    bool operator==(const Complexe& R) const;
+    double modulo(const Complexe& R);
+    Complexe conjuge() const;
+
+
 };
 
+ 
+
+#endif
