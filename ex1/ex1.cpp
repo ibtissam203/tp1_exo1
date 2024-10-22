66450211349cc8eaf8be27cd7c9b126d150da8f7
ibtissam abdouni <167817121+ibtissam203@users.noreply.github.com>
2024-10-22 11:16:51 +0000

    ex1.cpp

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -1,20 +1,45 @@
-// ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// tp1exo1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include "Complexe.h"
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Complexe c1(2, 3), c2(2, 4),c3, c4, c5 , c6;
+	cout << "c1 : ";
+
+	c1.afficher();
+	cout << "c2 : ";
+
+	c2.afficher();
+	c3 = c1 + c2;
+	cout << "c3 = c1 + c2 : ";
+	c3.afficher();
+
+	c4 = c1 - c2;
+	cout << "c4 = c1 - c2 : ";
+	c4.afficher();
+
+	c5 = c1 + 2;
+	cout << "c5 = c1 + 2 : ";
+	c5.afficher();
+
+	c6 = c1 * c2;
+	cout << "c6 = c1 + 2 : ";
+	c6.afficher();
+
+	Complexe c7(2, 3);
+	if (c1 == c7) {
+		cout << "c1 est egal a c7" << endl;
+	}
+	else {
+		cout << "c1 n'est pas egal a c7" << endl;
+	}
+
+	cout << "***************conjuge**************" << endl << endl;
+
+	c1.conjuge().afficher();
+
+	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
