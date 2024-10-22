a78de3e7e3151c912974705acec1a444c5273499
ibtissam abdouni <167817121+ibtissam203@users.noreply.github.com>
2024-10-22 11:15:25 +0000

    Complex.cpp

--- ex1/Complex.cpp
+++ ex1/Complex.cpp
@@ -1,49 +1,72 @@
-#include "Complex.h"
-using namespace std;
-Complex::Complex()
+#include "Complexe.h"
+#include <cmath>
+
+// Constructeur sans paramètre
+Complexe::Complexe()
 {
-	//std::cout << "constructeur sans parametre" << std::endl;
-	this->Re = 0;
-	this->Img = 0;
+    this->Re = 0;
+    this->Img = 0;
 }
 
-Complex::Complex(double reel, double imaginaire)
+// Constructeur avec paramètres
+Complexe::Complexe(double reel, double imaginaire)
 {
-	this->Re = reel;
-	this->Img = imaginaire;
+    this->Re = reel;
+    this->Img = imaginaire;
 }
 
-Complex::Complex(const Complex& c)
+// Constructeur de copie
+Complexe::Complexe(const Complexe& c)
 {
-	this->Re = c.Re;
-	this->Img = c.Img;
-
+    this->Re = c.Re;
+    this->Img = c.Img;
 }
 
-
-void Complex::afficher() const
+// Méthode d'affichage
+void Complexe::afficher() const
 {
-	cout << "la methode afficher" << endl;
-	cout << "complex=" << this->Re << "+ i" << this->Img << endl;
-}
-
-
-void Complex::afficher(double i) const
-{
-	cout << "la methode afficher" << endl;
-	cout << "complex=" << this->Re << "+ i" << this->Img << endl;
-}
-
-Complex Complex::operator+(const Complex& Z)
-{
-	return Complex(Re + Z.Re, Img + Z.Img);
+    
+    cout << "complex=" << this->Re << "+ " << this->Img << "i"<< endl << endl;
 }
 
 
-
-double Complex::modulo(Complex& R)
+// Surcharge de l'opérateur "+" pour Complexe + Complexe
+Complexe Complexe::operator+(const Complexe& Z) 
 {
-	return 0.0;
+    // Créer un nouvel objet Complexe avec la somme des parties réelles et imaginaires
+    return Complexe(Re + Z.Re, Img + Z.Img);
 }
 
+
+Complexe Complexe::operator+(const double a) const
+{
+    return Complexe(Re + a,Img);
+}
+
+
+
+Complexe Complexe::operator-(const Complexe& Z) const
+{
+    return Complexe(Re - Z.Re, Img - Z.Img);
+}
+
+Complexe Complexe::operator*(const Complexe& R) const
+{
+    return Complexe(Re * R.Re - Img * R.Img, Re * R.Img + Img * R.Re);
+}
+
+bool Complexe::operator==(const Complexe& R) const
+{
+    return (Re == R.Re) && (Img == R.Img);
+}
+
+double Complexe::modulo(const Complexe& R)
+{
+    return sqrt(pow(R.Re, 2) + pow(R.Img, 2));
+}
+
+Complexe Complexe::conjuge() const
+{
+    return Complexe(Re,-(Img));
+}
 
